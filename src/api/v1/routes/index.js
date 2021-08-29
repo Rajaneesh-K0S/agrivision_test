f34d71aa9dc40dbf5a298fc0b6663ec84e302027
chandan867 <58499594+chandan867@users.noreply.github.com>
2021-08-29 15:27:24 +0530

    Update index.js

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -9,7 +9,7 @@ const passport=require('passport')
 router.use('/user', require('./user'));
 
 
-router.use('/magazine',passport.authenticate('jwt',{session:false}) require('./magazine'));
+router.use('/magazine',passport.authenticate('jwt',{session:false}),require('./magazine'));
 
 router.get('/', (req, res) => {
     logger.debug('GET /v1');
