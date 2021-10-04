1e0aeec3c20a10a12b65fbddf41979e9a1adb5c3
s-prajapati <64549588+s-prajapati@users.noreply.github.com>
2021-10-04 16:38:52 +0530

    minor changes in dashboard api's (#34)
    * minor changes
    
    * dashboard api minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const passport = require("passport");
-const logger = require("./logger");
-const cors = require("cors");
-const streakCalculatorJob=require('./utils/scheduledJobs/streakCalculator')
+const passport = require('passport');
+const logger = require('./logger');
+const cors = require('cors');
+const streakCalculatorJob = require('./utils/scheduledJobs/streakCalculator');
 
-require("./config/mongoose");
-require("./config/auth/passport-google-token");
-require("./config/auth/passportJWT");
+require('./config/mongoose');
+require('./config/auth/passport-google-token');
+require('./config/auth/passportJWT');
 
 
 streakCalculatorJob.start();
@@ -17,27 +17,27 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 
-app.use("/", require("./api"));
+app.use('/', require('./api'));
 
 app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
 });
 
 // error handler middleware
-app.use((error, req, res, next) => {
-  res.status(error.status || 500).send({
-    error: {
-      status: error.status || 500,
-      message: error.message || "Internal Server Error",
-    },
-  });
+app.use((error, req, res) => {
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || 'Internal Server Error',
+        },
+    });
 });
 
 app.listen(process.env.PORT, (err) => {
-  if (err) {
-    logger.error(err);
-  }
-  logger.info("Server started");
+    if (err) {
+        logger.error(err);
+    }
+    logger.info('Server started');
 });
