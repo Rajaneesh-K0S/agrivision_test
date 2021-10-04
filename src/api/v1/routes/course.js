d54a431b79299f264d4d8a63c21f9ed0119e8221
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-10-04 19:20:45 +0530

    modified routes for course progress (#35)
    * added endpoint for payment page for a specific course
    
    * api endpoints added for cart operations
    
    * minor changes
    
    * updated routes for course progress

--- src/api/v1/routes/course.js
+++ src/api/v1/routes/course.js
@@ -6,6 +6,8 @@ const courseController = require('../controllers/courseController');
 
 router.get('/', courseController.allCourse );
 router.get('/:id', passport.authenticate('jwt', { session:false }), courseController.courseById);
+router.get('/:id/progress', passport.authenticate('jwt', { session:false }), courseController.courseProgress);
+router.post('/:id/markcompleted', passport.authenticate('jwt', { session:false }), courseController.markCompleted);
 router.get('/subtopics/:id', passport.authenticate('jwt', { session:false }), courseController.subTopics);
 
 module.exports = router;
