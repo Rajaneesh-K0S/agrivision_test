e33dcea0ea7eb77f4db50b79918e66047572bc08
s-prajapati <64549588+s-prajapati@users.noreply.github.com>
2021-10-08 01:51:28 +0530

    Update streakCalculator.js

--- src/utils/scheduledJobs/streakCalculator.js
+++ src/utils/scheduledJobs/streakCalculator.js
@@ -1,20 +1,20 @@
-const CronJob = require('cron').CronJob;
+// const CronJob = require('cron').CronJob;
 
-const { User } = require('../../models');
+// const { User } = require('../../models');
 
-var job = new CronJob('* * 1 * * *', async function() {
-    const user =await User.find({});
-    let date = new Date();
-    date.setDate(date.getDate() - 1);
-    user.forEach(user => {
-        if(user.readingDuration[user.readingDuration.length - 1].date == date.toLocaleString(undefined, { timeZone: 'Asia/Kolkata' }).split(',')[0]){
-            user.currentStreakDay++;
-        }else{
-            user.currentStreakDay = 0;
-        }
-        user.longestStreakDay = Math.max(user.longestStreakDay, user.currentStreakDay);
-    });
-    await user.save();
-}, null, true, 'Asia/Kolkata');
+// var job = new CronJob('* * 1 * * *', async function() {
+//     const user =await User.find({});
+//     let date = new Date();
+//     date.setDate(date.getDate() - 1);
+//     user.forEach(user => {
+//         if(user.readingDuration[user.readingDuration.length - 1].date == date.toLocaleString(undefined, { timeZone: 'Asia/Kolkata' }).split(',')[0]){
+//             user.currentStreakDay++;
+//         }else{
+//             user.currentStreakDay = 0;
+//         }
+//         user.longestStreakDay = Math.max(user.longestStreakDay, user.currentStreakDay);
+//     });
+//     await user.save();
+// }, null, true, 'Asia/Kolkata');
 
-module.exports = job;
+// module.exports = job;
