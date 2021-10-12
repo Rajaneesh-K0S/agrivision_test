6d2c984c5aab34a52e582934aa2b8a0ca3416472
s-prajapati <64549588+s-prajapati@users.noreply.github.com>
2021-10-12 15:10:32 +0530

    Update course.js

--- src/api/v1/routes/course.js
+++ src/api/v1/routes/course.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const courseController = require('../controllers/courseController');
 
 router.get('/', courseController.allCourse );
-router.get('/:id', courseController.courseById);
+router.get('/:id',passport.authenticate('jwt', { session:false }), courseController.courseById);
 router.get('/:id/progress', passport.authenticate('jwt', { session:false }), courseController.courseProgress);
 router.post('/:id/markcompleted', passport.authenticate('jwt', { session:false }), courseController.markCompleted);
 router.get('/subtopics/:id', passport.authenticate('jwt', { session:false }), courseController.subTopics);
