7a48a7e995ec1b69bdf2d09f329d29f60fdc2240
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-10-27 19:10:00 +0530

    Payment (#89)
    * payment endpoints modified
    
    * basics done
    
    * payment done
    
    * minor changes in quizcontroller
    
    * added some error checks

--- src/api/v1/routes/payment.js
+++ src/api/v1/routes/payment.js
@@ -4,9 +4,10 @@ const router = Router();
 const { order, success } = require('../controllers/paymentController');
 const passport = require('passport');
 
-router.post('/:amount/:courseId', passport.authenticate('jwt', { session:false }), order);
-
 router.post('/success', success);
+router.post('/:amount/', passport.authenticate('jwt', { session:false }), order);
+
+
 
 
 module.exports = router;
