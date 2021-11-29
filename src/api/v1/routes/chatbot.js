25b942698acf7e5fbb16b3b42cd187017caeef50
s-prajapati <64549588+s-prajapati@users.noreply.github.com>
2021-11-29 18:53:55 +0530

    Revert "Chatbot controller and md added (#124)" (#125)
    This reverts commit 67c58072770b037c47c2259db46480698b879e33.

--- src/api/v1/routes/chatbot.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { getResponseFromDialogflow } = require('../controllers/chatbotController');
-
-router.post('/get-response', getResponseFromDialogflow);
-module.exports = router;
