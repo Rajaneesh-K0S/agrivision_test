8807a13f30491557b485a1cb97d5d53ed62404bb
Gourav Dangi <63147754+gouravdangi@users.noreply.github.com>
2021-12-04 23:23:44 +0530

    chatbot tested (#140)

---
+++ src/api/v1/controllers/chatbotController.js
@@ -0,0 +1,62 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+
+
+async function getResponseFromDialogflow(data, projectId = 'newagent-fbpr') {
+    // A unique identifier for the given session
+    const sessionId = uuid.v4();
+    const pth = __dirname + "/newagent-fbpr-00b4b2bbd863.json";
+    // Create a new session
+    const sessionClient = new dialogflow.SessionsClient(
+        {
+            keyFilename: pth
+        }
+    );
+    const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+    );
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                // The query to send to the dialogflow agent
+                text: data,
+                // The language used by the client (en-US)
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    let obj = [];
+    obj.push(responses[0].queryResult.fulfillmentText);
+    obj.push(responses[0].queryResult.intent.displayName);
+    return obj;
+}
+
+module.exports.getResult = async (req, res) => {
+    try {
+        const data = req.body.data;
+        const result = await getResponseFromDialogflow(data);
+        res.status(200).json({
+            data: { ...result },
+            success: true
+        });
+        // res.send("hello world")
+    }
+    catch (err) {
+        res.status(500).json({
+            data: err,
+            success: False
+        })
+    }
+}
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+
--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -9,15 +9,15 @@ const passport = require('passport');
 router.use('/user', require('./user'));
 router.use('/quiz', require('./quiz'));
 router.use('/course', require('./course'));
-router.use('/package',require('./package'));
-router.use('/jobs',  require('./job'));
+router.use('/package', require('./package'));
+router.use('/jobs', require('./job'));
 router.use('/testseries', require('./testSeries'));
-
+router.use('/chatbot', require('./chatbot'));
 router.use('/payment', require('./payment'));
-router.use('/article',require('./article'));
+router.use('/article', require('./article'));
 router.use('/coupens', require('./coupen'));
 
-router.use('/magazine',  require('./magazine'));
+router.use('/magazine', require('./magazine'));
 router.use('/academics', require('./academics'));
 
 router.get('/', (req, res) => {
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
 router.get('*', (req, res) => {
     res.status(404).json({
         message: 'Page Not Found',
-        success:false
+        success: false
     });
 });
 
