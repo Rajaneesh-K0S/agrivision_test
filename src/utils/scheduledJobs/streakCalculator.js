49eb2a2ece46b36abb8095bfe6d6488d9d1bd1d1
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-12-13 11:35:27 +0530

    bug fixed in streak calculator (#147)

--- src/utils/scheduledJobs/streakCalculator.js
+++ src/utils/scheduledJobs/streakCalculator.js
@@ -2,29 +2,35 @@ const CronJob = require('cron').CronJob;
 const { User } = require('../../models');
 const { getLocalTimeString } = require('../');
 var streakCalculatorJob = new CronJob('0 0 1 * * *', async function () {
-    try{
+    try {
         console.log("hi everynight 1 am");
         const users = await User.find({});
-        let currentDate = getLocalTimeString(new Date());
         let previousDate = new Date();
         previousDate.setDate(previousDate.getDate() - 1);
         previousDate = getLocalTimeString(previousDate);
         for (let i = 0; i < users.length; i++) {
             let user = users[i];
-            let len = user.readingDuration.length
             let readingDuration = user.readingDuration;
-            if (len) {
-                if (readingDuration[len - 1].date == previousDate || readingDuration[len - 1].date == currentDate) {
+            let testDuration = user.testDuration;
+
+            let prevDateReadingDuration = readingDuration.filter(obj => obj.date == previousDate);
+            if (prevDateReadingDuration.length) {
+                user.currentStreakDay++;
+            }
+            else{
+                let prevDateTestDuration = testDuration.filter(obj => obj.date == previousDate);
+                if (prevDateTestDuration.length) {
                     user.currentStreakDay++;
                 } else {
                     user.currentStreakDay = 0;
                 }
-                user.longestStreakDay = Math.max(user.longestStreakDay, user.currentStreakDay);
-                await user.save();
             }
+
+            user.longestStreakDay = Math.max(user.longestStreakDay, user.currentStreakDay);
+            await user.save();
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 }, null, true, 'Asia/Kolkata');
