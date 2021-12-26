dcb63b3fe7f9bda505c2123e32555287a98a9c76
Gourav Dangi <63147754+gouravdangi@users.noreply.github.com>
2021-12-26 22:03:55 +0530

    Specific college search function (#157)

--- src/api/v1/controllers/academicController.js
+++ src/api/v1/controllers/academicController.js
@@ -45,3 +45,32 @@ module.exports.collegeDetails = async(req,res)=>{
         });
     }
 }
+
+module.exports.customSearch = async (req, res) => {
+    const { c_name, state } = req.body;
+    //other search parameters can be added
+    //location: state, courseFeesCategory: { $lt: money },
+    try {
+        let colleges = await College.find({ name: new RegExp('.*' + c_name + '.*'), location: new RegExp('.*' + state + '.*') });
+        //console.log('colleges : ');
+        //console.log(colleges);
+        //console.log(c_name);
+        const domain = req.query.domain;
+        if (domain) {
+            colleges = await College.find({ domain: domain });
+        }
+        res.status(200).json({
+            data: colleges,
+            message: 'colleges sent according to query',
+            success: true
+        });
+
+    }
+    catch (error) {
+        res.status(400).json({
+            error: error.message,
+            message: 'Something went wrong',
+            success: false
+        });
+    }
+};
