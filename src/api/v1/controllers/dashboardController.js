fdefb2b9ff4b8686c4788b791520a404df786b3b
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2022-02-13 13:21:26 +0530

    last completed subtopic bug fix in dashboard  (#170)
    * temp commit
    
    * Revert "temp commit"
    
    This reverts commit d5cd0b0cd4da84a24c5532b674fa6d067c4b2ba5.
    
    * fixed last completed subtopic bug in dashboard

--- src/api/v1/controllers/dashboardController.js
+++ src/api/v1/controllers/dashboardController.js
@@ -1,122 +1,154 @@
-const {User, Course, Chapter, Topic, SubTopic, TestSeries, Package, Event}  = require("../../../models");
-const {getLocalTimeString} = require("../../../utils");
+const { User, Course, Chapter, Topic, SubTopic, TestSeries, Package, Event } = require("../../../models");
+const { getLocalTimeString } = require("../../../utils");
 
-module.exports.addReminder = async function(req, res){
+
+let initializeLastCompleted = async (req, courseId) => {
+    let course = await Course.findOne({ _id: courseId }, { '_id': 1 }).populate({ path: 'chapters', select: '_id', populate: { path: 'topics', select: '_id subTopics' } });
+    let chapters = course.chapters;
+    let chapterId, subTopicId, topicId;
+    if (chapters.length) {
+        chapterId = chapters[0]._id;
+    }
+    let topics = chapters[0].topics;
+    if (topics.length) {
+        topicId = topics[0]._id;
+    }
+    if (topics[0].subTopics.length) {
+        subTopicId = topics[0].subTopics[0];
+    }
+    await User.updateOne({ _id: req.user._id }, { lastCompleted: subTopicId });
+    lastCompleted = { courseId, chapterId, topicId, subTopicId };
+    return lastCompleted;
+}
+
+
+module.exports.addReminder = async function (req, res) {
     try {
         let user = await User.findById(req.user._id);
         let reminder = {
-            task:req.body.task,
-            date:req.body.date
+            task: req.body.task,
+            date: req.body.date
         };
         user.reminder.push(reminder);
         await user.save();
         return res.status(200).json({
-            success:true,
-            message:'reminder added successfully'
+            success: true,
+            message: 'reminder added successfully'
         });
     } catch (error) {
         return res.status(500).json({
-            success:false,
+            success: false,
             message: error.message
         });
     }
 };
 
-module.exports.getReminder = async function(req, res){
+module.exports.getReminder = async function (req, res) {
     try {
         let user = await User.findById(req.user._id);
         const data = user.reminder;
         return res.status(200).json({
-            data:data,
-            success:true
+            data: data,
+            success: true
         });
     } catch (error) {
         res.status(500).json({
-            message : error.message,
-            success : false
+            message: error.message,
+            success: false
         });
     }
 };
 
 
-module.exports.getUserProgress = async function(req, res){
+module.exports.getUserProgress = async function (req, res) {
     try {
         let user = await User.findById(req.user._id);
         let todayDate = getLocalTimeString(new Date());
-        let testsCompleted= 0, totalTimeSpent = 0;
-        user.readingDuration.forEach(dur=>{
-            totalTimeSpent += (dur.duration)?(dur.duration):0;
-            totalTimeSpent += (dur.watchDuration)?(dur.watchDuration):0;
+        let testsCompleted = 0, totalTimeSpent = 0;
+        user.readingDuration.forEach(dur => {
+            totalTimeSpent += (dur.duration) ? (dur.duration) : 0;
+            totalTimeSpent += (dur.watchDuration) ? (dur.watchDuration) : 0;
         })
-        user.testDuration.forEach(dur=>{
-            testsCompleted += (dur.testsCompleted)?(dur.testsCompleted):0;
-            totalTimeSpent += (dur.duration)?(dur.duration):0;
+        user.testDuration.forEach(dur => {
+            testsCompleted += (dur.testsCompleted) ? (dur.testsCompleted) : 0;
+            totalTimeSpent += (dur.duration) ? (dur.duration) : 0;
         })
         const data = {
-            readingDuration:user.readingDuration,
-            testDuration:user.testDuration,
-            minutesGoal:user.minutesGoal,
-            readingGoal:user.readingGoal,
-            currentStreakDay:user.currentStreakDay,
-            longestStreakDay:user.longestStreakDay,
+            readingDuration: user.readingDuration,
+            testDuration: user.testDuration,
+            minutesGoal: user.minutesGoal,
+            readingGoal: user.readingGoal,
+            currentStreakDay: user.currentStreakDay,
+            longestStreakDay: user.longestStreakDay,
         };
         data['testsCompleted'] = testsCompleted;
         data['totalTimeSpent'] = totalTimeSpent;
-        if(user.readingDuration.filter(obj=> obj.date == todayDate).length){
-            data['todayReadingDuration'] = user.readingDuration.filter(obj=> obj.date == todayDate)[0].duration;
-        }else{
+        if (user.readingDuration.filter(obj => obj.date == todayDate).length) {
+            data['todayReadingDuration'] = user.readingDuration.filter(obj => obj.date == todayDate)[0].duration;
+        } else {
             data['todayReadingDuration'] = 0;
         }
-        if(user.testDuration.filter(obj=> obj.date == todayDate).length){
-            data['todayTestCompleted'] = user.testDuration.filter(obj=> obj.date == todayDate)[0].testsCompleted;
-        }else{
+        if (user.testDuration.filter(obj => obj.date == todayDate).length) {
+            data['todayTestCompleted'] = user.testDuration.filter(obj => obj.date == todayDate)[0].testsCompleted;
+        } else {
             data['todayTestsCompleted'] = 0;
         }
         let lastCompletedSubTopic = user.lastCompleted;
         let lastCompleted = {};
-        if(lastCompletedSubTopic){
+        if (lastCompletedSubTopic) {
             let topic, chapter, course;
-            topic = await Topic.findOne({"subTopics" : lastCompletedSubTopic}, {"_id" : 1});
-            if(topic){
-                chapter = await Chapter.findOne({"topics" : topic._id}, {"_id" : 1});
+            topic = await Topic.findOne({ "subTopics": lastCompletedSubTopic }, { "_id": 1 });
+            if (topic) {
+                chapter = await Chapter.findOne({ "topics": topic._id }, { "_id": 1 });
             }
-            if(chapter){
-                course = await Course.findOne({"chapters" : chapter._id}, {"_id" : 1});
+            if (chapter) {
+                course = await Course.findOne({ "chapters": chapter._id }, { "_id": 1 });
             }
-            if(course){
-                lastCompleted = {courseId : course._id, chapterId : chapter._id, topicId : topic._id, subTopicId : lastCompletedSubTopic};
+            if (course) {
+                if (!user.courses.includes(course)) {
+                    if(user.courses.length){
+                        lastCompleted = await initializeLastCompleted(req, user.courses[0]);
+                    }else{
+                        await User.updateOne({_id : req.user._id}, {"$unset":{lastCompleted : ''}})
+                    }
+                }else{
+                    lastCompleted = { courseId: course._id, chapterId: chapter._id, topicId: topic._id, subTopicId: lastCompletedSubTopic };
+                }
             }
+        } else if (user.courses.length != 0) {
+            let courseId = user.courses[0];
+            lastCompleted = await initializeLastCompleted(req, courseId);
         }
         data['lastCompleted'] = lastCompleted;
-        
+
         return res.status(200).json({
-            data:data,
-            success:true
+            data: data,
+            success: true
         });
     } catch (error) {
         res.status(500).json({
-            message : error.message,
-            success : false
+            message: error.message,
+            success: false
         });
     }
 };
 
 
-module.exports.getSchedule = async (req, res)=>{
-    try{
+module.exports.getSchedule = async (req, res) => {
+    try {
         let userCourses = req.user.courses;
         let todayDate = getLocalTimeString(new Date());
         console.log(todayDate);
-        let events = await Event.find({date: todayDate});
+        let events = await Event.find({ date: todayDate });
         let data = [];
-        userCourses.forEach(courseId=>{
-            let courseEvents = events.filter(obj=> obj.relatedCourses.includes(courseId));
+        userCourses.forEach(courseId => {
+            let courseEvents = events.filter(obj => obj.relatedCourses.includes(courseId));
             data = data.concat(courseEvents);
         })
-        let allEvents = events.filter(obj=> obj.eventForAll == true);
+        let allEvents = events.filter(obj => obj.eventForAll == true);
         data = data.concat(allEvents);
         data = Array.from(new Set(data));
-        data.forEach((event, i)=>{
+        data.forEach((event, i) => {
             event = event.toJSON();
             let startDate = new Date(event.startTime);
             let endDate = new Date(event.endTime);
@@ -126,37 +158,37 @@ module.exports.getSchedule = async (req, res)=>{
         })
         res.status(200).json({
             data,
-            message : "successfully fetched events data",
-            success : true
+            message: "successfully fetched events data",
+            success: true
         })
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message : err.message,
-            success : false
+            message: err.message,
+            success: false
         })
     }
 }
 
-module.exports.setGoal = async (req, res)=>{
-    try{
-        let {readingGoal, watchingGoal} = req.body;
+module.exports.setGoal = async (req, res) => {
+    try {
+        let { readingGoal, watchingGoal } = req.body;
         let userId = req.user._id;
-        if(readingGoal){
-            await User.updateOne({_id : userId}, {readingGoal});
+        if (readingGoal) {
+            await User.updateOne({ _id: userId }, { readingGoal });
         }
-        if(watchingGoal){
-            await User.updateOne({_id : userId}, {minutesGoal:watchingGoal});
+        if (watchingGoal) {
+            await User.updateOne({ _id: userId }, { minutesGoal: watchingGoal });
         }
         res.status(200).json({
-            message : "Successfully updated goals",
-            success : true
+            message: "Successfully updated goals",
+            success: true
         })
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            message : err.message,
-            success : false
+            message: err.message,
+            success: false
         })
     }
 }
