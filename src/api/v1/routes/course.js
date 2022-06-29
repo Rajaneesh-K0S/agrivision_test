ef4f0d519ebe4cdb4ecb6ed9a5ff2c735285adcb
Abhiram-003 <81378619+Abhiram-003@users.noreply.github.com>
2022-06-29 11:05:09 +0530

    added live class route
    done

--- src/api/v1/routes/course.js
+++ src/api/v1/routes/course.js
@@ -7,5 +7,6 @@ router.get('/', courseController.allCourse );
 router.get('/:id', passport.authenticate('jwt', { session:false }), courseController.courseById);
 router.post('/:id/markcompleted', passport.authenticate('jwt', { session:false }), courseController.markCompleted);
 router.get('/subtopics/:id', passport.authenticate('jwt', { session:false }), courseController.subTopics);
+router.get('/liveclass/:id', passport.authenticate('jwt', { session:false }), courseController.liveClass);
 
 module.exports = router;
