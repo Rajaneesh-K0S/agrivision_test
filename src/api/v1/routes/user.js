dfe1f3c9b850e72becb003d557b6b7ac43d29116
Sarthak Bhagwat <sarthak_bhagwat@pop-os.localdomain>
2022-06-30 21:16:42 +0530

    added subject field to getProfile

--- src/api/v1/routes/user.js
+++ src/api/v1/routes/user.js
@@ -1,23 +1,64 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const passport = require('passport');
-const { uploadImg } = require('../../../config/imageUpload');
+const passport = require("passport");
+const { uploadImg } = require("../../../config/imageUpload");
 
-const { registerUser, login, resendMail, googleOauth, googleOneTapLogin, confirmEmail, resetPassword, forgotPassword, getCart, deleteProductInCart, addToCart, getReminder, userProgress, addReminder, getProfile, updateProfile} = require('../controllers/userController');
+const {
+  registerUser,
+  login,
+  resendMail,
+  googleOauth,
+  googleOneTapLogin,
+  confirmEmail,
+  resetPassword,
+  forgotPassword,
+  getCart,
+  deleteProductInCart,
+  addToCart,
+  getReminder,
+  userProgress,
+  addReminder,
+  getProfile,
+  updateProfile,
+} = require("../controllers/userController");
 
+router.post("/register", registerUser);
 
-router.post('/register', registerUser);
-
-router.post('/login', login);
-router.post('/oauth/google', passport.authenticate('google-token', { session: false }), googleOauth);
-router.post('/oneTapLogin', googleOneTapLogin);
-router.get('/resendMail',resendMail);
-router.get('/confirmEmail', confirmEmail);
-router.post('/forgotPassword', forgotPassword);
-router.post('/resetPassword', resetPassword);
-router.get('/cart/:id', passport.authenticate('jwt', { session:false }), getCart);
-router.delete('/cart/:id', passport.authenticate('jwt', { session:false }), deleteProductInCart);
-router.post('/cart/:id', passport.authenticate('jwt', { session:false }), addToCart);
-router.get('/profile', passport.authenticate('jwt', { session:false }), getProfile);
-router.post('/profile', passport.authenticate('jwt', { session:false }), uploadImg.single('image'), updateProfile);
+router.post("/login", login);
+router.post(
+  "/oauth/google",
+  passport.authenticate("google-token", { session: false }),
+  googleOauth
+);
+router.post("/oneTapLogin", googleOneTapLogin);
+router.get("/resendMail", resendMail);
+router.get("/confirmEmail", confirmEmail);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
+router.get(
+  "/cart/:id",
+  passport.authenticate("jwt", { session: false }),
+  getCart
+);
+router.delete(
+  "/cart/:id",
+  passport.authenticate("jwt", { session: false }),
+  deleteProductInCart
+);
+router.post(
+  "/cart/:id",
+  passport.authenticate("jwt", { session: false }),
+  addToCart
+);
+router.get(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  getProfile
+);
+router.post(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  uploadImg.single("image"),
+  updateProfile
+);
 module.exports = router;
